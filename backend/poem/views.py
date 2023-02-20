386fecb34245ccb05b5644de3acf8d10e911630c
Sam <giratina5055@gmail.com>
2023-02-20 11:56:38 +0000

    created api

--- backend/poem/views.py
+++ backend/poem/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
+from rest_framework import viewsets
+from .serializers import PoemSerializer
+from .models import Poem
 # Create your views here.
+
+
+
+class PoemView(viewsets.ModelViewSet):
+    serializer_class = PoemSerializer
+    queryset = Poem.objects.all()
