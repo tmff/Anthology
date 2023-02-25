50696ef8c45531de789cdf0af4f9c76b94865f92
Holly Packer <hjp183@student.bham.ac.uk>
2023-02-25 13:35:10 +0000

    fix: use tags

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,9 +1,9 @@
 
 echo "Building the backend image..."
-docker build -t teamproject_backend ../backend/
+docker build -t teamproject:backend ../backend/
 
 echo "Building the frontend image..."
-docker build -t teamproject_frontend ../poemfrontend
+docker build -t teamproject:frontend ../poemfrontend
 
 echo "Running the composer..."
 docker compose -f ../docker/app.yml up
