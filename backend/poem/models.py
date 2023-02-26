05bc6ba391b2129ae6f2818498ae9d0ecf2bf6b0
Holly Packer <hjp183@student.bham.ac.uk>
2023-02-26 22:42:14 +0000

    feat: add new models

--- backend/poem/models.py
+++ backend/poem/models.py
@@ -1,9 +1,74 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Count, Q
 
-# Create your models here.
+import datetime
+
+GHOST_USER = User()
+
+"""
+Represents a poem.
+
+Consists of the following:
+- Poem title: the title of the poem.
+- Poem content: the actual poem content.
+- Author: the author of the poem as a User object.
+- Time created: when the poem was submitted to the database.
+"""
 class Poem(models.Model):
+
     title = models.CharField(max_length=120)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # TODO - should not be null but it's half 10 in the evening and i need sleep
+    time_created = models.DateTimeField(default=datetime.datetime.now)
+
 
     def __str__(self):
         return self.title
+    
+
+    def get_author(self) -> User:
+        return self.author
+    
+
+    def get_content(self) -> str:
+        return self.content
+    
+
+    def get_title(self) -> str:
+        return self.title
+    
+
+    def get_bookmark_count(self) -> int:
+        return Bookmark.objects.aggregate(count = Count('pk', filter=Q(poem=self)))["count"]
+    
+
+    def get_comment_count(self) -> int:
+        return Comment.objects.aggregate(count = Count('pk', filter=Q(poem=self)))["count"]
+
+
+class Bookmark(models.Model):
+ 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Do we replace with a "ghost" user?
+    poem = models.ForeignKey(Poem, on_delete=models.CASCADE)
+
+
+    def get_user(self) -> User:
+        return self.user
+    
+
+    def get_poem(self) -> Poem:
+        return self.poem
+    
+
+class Comment(models.Model):
+
+    poem = models.ForeignKey(Poem, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+
+class Reply(Comment):
+
+    parent_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='parent_comment_to_reply')
+
--- backend/poem/serializers.py
+++ backend/poem/serializers.py
@@ -6,39 +6,52 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 from .models import Poem
 
+
 class PoemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poem
-        fields = ('id','title','content')
+        fields = ('id','title','content','author','time_created')
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id','first_name','last_name','username']
 
+
 class RegisterSerializer(serializers.ModelSerializer):
+
+    # Email field
     email = serializers.EmailField(
         required=True,
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
+
+    # The chosen password
     password =  serializers.CharField(
         write_only=True,
         required=True,validators=[validate_password],
         style={'input_type': 'password', 'placeholder': 'Password'},
     )
+
+    # Repeating the password
     password2 = serializers.CharField(write_only=True, required=True,style={'input_type': 'password', 'placeholder': 'Confirm Password'},)
+
+    # The metadata for the serialiser
     class Meta:
         model = User
         fields = ('username', 'password', 'password2',
          'email', 'first_name', 'last_name')
         extra_kwargs = {'first_name': {'required': True},'last_name': {'required': True}}
 
+    # Perform password validation
     def validate(self,attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
         else:
             return attrs
     
+    # Create the user
     def create(self,validated_data):
         user = User.objects.create(
             username = validated_data['username'],
