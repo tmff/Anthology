8ab12c60f5ccf630c3ae94f68bb48eada9bd4033
awoodham11 <alexwoodham08@gmail.com>
2023-02-27 11:30:20 +0000

    added search component

---
+++ poemfrontend/src/components/Search.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+  
+
+
+const Search = (props) => {
+  const [searchItem, setSearchItem] = useState('')
+  const [searchErrorText, setSearchErrorText] = useState('')
+
+  function handleUserClick(){
+    if(searchItem.length === 0){
+      setSearchErrorText('Nothing found')
+    }
+    else{
+      setSearchItem('Users here')
+    }
+  }
+
+  
+  function handlePostsClick(){
+    if(searchItem.length === 0){
+      setSearchErrorText('Nothing found')
+    }
+    else{
+      setSearchItem('Posts here')
+    }
+  }
+  
+  return (
+    <div className="container">
+      <h2>Search</h2>
+      <div className='search'>
+        <input
+          placeholder='Search for users or posts'
+          onChange={() => {}}
+          value={searchItem}
+          type="text"
+        />
+        <button 
+          className="user-button"
+          onClick={handleUserClick}
+        >   Users
+        </button>
+        <button 
+          className="posts-button"
+          onClick={handlePostsClick}
+        >   Posts
+        </button>
+      </div>
+      <h3>{searchItem}</h3>
+      <h3>{searchErrorText}</h3>
+      <h2>Daily Themes</h2>
+      <p> Themes will go here.</p>
+    </div>
+  );
+}
+  
+export default Search;
