285cabd3b66706bbbe5ae0b72a8beb3bf3dc2e75
Sam <giratina5055@gmail.com>
2023-03-02 15:36:33 +0000

    poemview

---
+++ poemfrontend/src/components/PoemView.jsx
@@ -0,0 +1,10 @@
+import { useState} from 'react';
+import Cookies from 'universal-cookie';
+
+export const Writer = (props) => {
+    return(
+        <div>
+            <h1>umm</h1>
+        </div>
+    )
+}
