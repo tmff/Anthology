3c86f890b2af486f9dbb9b09e1e12f45af690668
Holly Packer <hjp183@student.bham.ac.uk>
2023-03-02 13:02:46 +0000

    fix: try using the router instead

--- backend/poem/urls.py
+++ backend/poem/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework import routers
 from .views import UserDetailAPI,RegisterUserAPIView, PoemView
 
+router = routers.DefaultRouter()
+router.register(r'poems', PoemView)
+
 urlpatterns = [
+  path("", include(router.urls)),
   path("get-details", UserDetailAPI.as_view()),
   path("register", RegisterUserAPIView.as_view()),
-  path("poems", PoemView.as_view()),
 ]
