644466bb851e9eef6556f24074c0cbb7addb8730
Holly Packer <hjp183@student.bham.ac.uk>
2023-03-02 12:49:36 +0000

    feat: add poems view to API

--- backend/poem/urls.py
+++ backend/poem/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserDetailAPI,RegisterUserAPIView
+from .views import UserDetailAPI,RegisterUserAPIView, PoemView
+
 urlpatterns = [
-  path("get-details",UserDetailAPI.as_view()),
-  path('register',RegisterUserAPIView.as_view()),
+  path("get-details", UserDetailAPI.as_view()),
+  path("register", RegisterUserAPIView.as_view()),
+  path("poems", PoemView.as_view()),
 ]
