b19c567e91c209deb76c53d958a74c9ea4cf0038
Sam Roberts <tmf@SamsPC>
2023-03-05 19:48:36 +0000

    added profiles and friends list

--- backend/poem/admin.py
+++ backend/poem/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import Poem,Tag
+from .models import Poem,Tag,Profile
 # Register your models here.
 
 class PoemAdmin(admin.ModelAdmin):
@@ -9,5 +11,15 @@ class PoemAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
 admin.site.register(Poem,PoemAdmin)
 admin.site.register(Tag,TagAdmin)
+admin.site.register(User,UserAdmin)
