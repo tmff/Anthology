e01bf5a866afef3e209ff0f87607dac72f1615f2
Omar Ahmed <oxa193@student.bham.ac.uk>
2023-03-08 14:10:32 +0000

    add: infinite scroll bar

--- poemfrontend/src/components/pages/Friends.jsx
+++ poemfrontend/src/components/pages/Friends.jsx
@@ -17,7 +17,7 @@ const fetchData = (setItems, items) => {
 const refresh = (setItems) => {};
 
 export const Friends = (props) => {
-    
+
     const [items, setItems] = useState([]);
 
     useEffect(() => {
@@ -25,8 +25,26 @@ export const Friends = (props) => {
     },[])
 
     return (
+        <InfiniteScroll
+            dataLength={items.length}
+            next={fetchData}
+            hasMore={true}
+            loader={<h4 className='load-msg'>Loading...</h4>}
+            endMessage= {
+                <p className='end-msg'>
+                    <b>You have reached the end!</b>
+                </p>
+            }
+            refreshFunction={this.refresh}
+            pullDownToRefresh
+            pullDownToRefreshThreshold={50}
+            pullDownToRefreshContent={
+                <h3 className='pulldown-msg'>&#8595; Pull down to refresh!</h3>
+            }
+        >
         <div className="friends">
-            <p>Hello</p>
+            
         </div>
+        </InfiniteScroll>
     )
 }
