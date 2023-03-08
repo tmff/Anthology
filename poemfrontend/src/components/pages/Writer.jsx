17d2057243d3d4d80cc016ce265ab9a996a8a8b0
Holly Packer <hjp183@student.bham.ac.uk>
2023-03-08 09:46:49 +0000

    feat: use react router

--- poemfrontend/src/components/pages/Writer.jsx
+++ poemfrontend/src/components/pages/Writer.jsx
@@ -1,7 +1,7 @@
-import api from "../js/Api"
+import api from "../../js/Api"
 import { useState} from 'react';
 import Cookies from 'universal-cookie';
-import '../css/Poem.css'
+import '../../css/Poem.css'
 
 export const Writer = (props) => {
     const [title,setTitle] = useState('');
--- poemfrontend/src/index.js
+++ poemfrontend/src/index.js
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import App from './App';
+import App, { loader as appLoader } from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Title } from './components/Greeting';
+import { Base } from './components/Base';
+import { Login } from './components/pages/Login';
+import { Register } from './components/pages/Register';
+import { Friends } from './components/pages/Friends';
+import { Writer } from './components/pages/Writer';
+import { Search } from './components/pages/Search';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    loader: appLoader,
+  },
+  {
+    path: "login",
+    element: <Title component={ <Login /> }></Title> 
+  },
+  {
+    path: "register",
+    element: <Title component={ <Register /> }></Title>
+  },
+  {
+    path: "friends",
+    element: <Base component={ <Friends /> }></Base>
+  },
+  {
+    path: "write",
+    element: <Base component={ <Writer /> }></Base>
+  },
+  {
+    path: "search",
+    element: <Base component={ <Search /> }></Base>
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ router } />
   </React.StrictMode>
 );
 
