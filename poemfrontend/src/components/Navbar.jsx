d7b7b72954c6698d42908ee20678d4f9efb5ff4e
Omar Ahmed <oxa193@student.bham.ac.uk>
2023-03-09 20:14:59 +0000

    fix: pipeling

--- poemfrontend/src/components/Navbar.jsx
+++ poemfrontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/Navbar.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
