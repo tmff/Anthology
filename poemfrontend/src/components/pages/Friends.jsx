8f7f0cf6e2c057bd7db669eecf41bbf1f79e99a4
Omar Ahmed <oxa193@student.bham.ac.uk>
2023-03-09 01:18:56 +0000

    fix: WCAG accessibility standards

--- poemfrontend/src/components/pages/Friends.jsx
+++ poemfrontend/src/components/pages/Friends.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import InfiniteScroll from "react-infinite-scroll-component";
+import '../../css/Friends.css';
 
 import api from '../../js/Api.js';
 
@@ -17,35 +18,17 @@ const fetchData = (setItems, items) => {
 const refresh = (props) => {};
 
 export const Friends = (props) => {
-
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        fetchData(setItems, items);
-    },[])
+    const [data, setData] = useState(Array.from({length:20}))
 
     return (
-        <InfiniteScroll
-            dataLength={items.length}
-            next={fetchData}
-            hasMore={true}
-            loader={<h4 className='load-msg'>Loading...</h4>}
-            endMessage= {
-                <p className='end-msg'>
-                    <b>You have reached the end!</b>
-                </p>
-            }
-            // TODO - refresh seems to be breaking things.
-            // refreshFunction={this.refresh}
-            // pullDownToRefresh
-            // pullDownToRefreshThreshold={50}
-            // pullDownToRefreshContent={
-            //     <h3 className='pulldown-msg'>&#8595; Pull down to refresh!</h3>
-            // }
-        >
-        <div className="friends">
-            
+        <div className='friends'>
+            <InfiniteScroll
+                dataLength={data.length}
+            >
+                {data.map((item,index) => {
+                    return <div>This is div #{index+1}</div>
+                })}
+            </InfiniteScroll>
         </div>
-        </InfiniteScroll>
-    ) 
+    )
 }
