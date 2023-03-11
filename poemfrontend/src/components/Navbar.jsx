cba39bc0477ae9c8d1d9213a3c6114c47ff35821
Omar Ahmed <oxa193@student.bham.ac.uk>
2023-03-11 17:01:02 +0000

    fix: border on log/reg button

--- poemfrontend/src/components/Navbar.jsx
+++ poemfrontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/Navbar.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
@@ -10,6 +10,7 @@ function AnthNavbar() {
             <Navbar fixed='top'>
                 <Container>
                     <Navbar.Brand href='/friends'>
+                        {/* <img> link of app logo can go here */}
                         <h1 className='app-title'>Anthology</h1>
                     </Navbar.Brand>
                 </Container>
