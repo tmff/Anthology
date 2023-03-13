a90e78d95e3e1c0b150281a5f39aab417ad92291
awoodham11 <alexwoodham08@gmail.com>
2023-03-13 19:08:50 +0000

    Added GDPR page

---
+++ poemfrontend/src/components/pages/GDPR.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const GDPR = () => {
+    return(
+        <div>
+            <h1 className='GDPR-Title'>Anthology GDPR Policy</h1>
+            <p>This privacy policy will explain how our organisation uses the personal
+                 data we collect from you when you use our website. 
+                 This policy is linked on our registration page and 
+                 is always available in the footer of our application.  </p>
+            <h2>What data do we collect?</h2>
+            <p>Anthology collects the following data:</p>
+            <ul>
+                <li>Personal identification information (Full name, email address, password, etc.) </li>
+                <li>Photo for profile picture if uploaded  </li>
+                <li>Users that you are linked with via a ”Friends” feature </li>
+                <li>Location when using the app or always as agreed to by the user </li>
+                <li>Voice recording of poem if uploaded  </li>
+            </ul>
+            <h2>How do we collect your data?</h2>
+            <p>You directly provide Anthology with most of the data we collect.
+                 We collect data and process data when you: </p>
+            <ul>
+                <li>Register for our app and you submit 
+                    personal identification information to create an account </li>
+                <li>Use or view our application with your browsers cookies </li>
+                <li>By using your device's inbuilt location tracker  </li>
+                <li>Voice recordings are collected if the user decides to upload a reading of their poem  </li>
+            </ul>
+            <h2>How will we use your data?</h2>
+            <p>Anthology collects your data so that we can: </p>
+            <ul>
+                <li>Log your poems so you can always have access to your previous work </li>
+                <li>Process your poems using Natural Language Toolkit to understand the sentiment so that 
+                    The Reading Room (discovery page) can have more personalised suggestions of poems that you are more likely to enjoy. </li>
+                <li>Log your friends on the app so we can show the correct poems on the home page for each user  </li>
+                <li>Log the locations so that we can see where our app is most used to have a better understanding of our users and how we can improve the experience to retain or attract new users </li>
+                <li>Voice recordings are displayed alongside the poem to allow other users to hear the poem from the author themselves and there is no further use for them.  </li>
+            </ul>
+            <h2>How do we store your data?</h2>
+            <ul>
+                <li>Anthology securely stores your data at University of Birmingham and only the IT team has secure limited access to data </li>
+                <li>Anthology will keep your personal data and cookie history for 9 months (till the end of 2023).
+                     Once this time period has expired, we will delete your data by clearing the database. </li>
+            </ul>
+            <h2>How do we use cookies?</h2>
+            <p>Anthology uses cookies in a range of ways to improve your experience on our website, including:  </p>
+            <ul>
+                <li>Functionality cookies so our app recognises you and keeps you signed in while using our app. 
+                    This allows the application to display your poems and friends and remember your previously selected preferences such as language, 
+                    light/dark mode and preference to autoplay poems for accessibility etc.  </li>
+            </ul>
+            <p>You can manage cookies by not accepting additional cookies on your browser, however this can limit the experience.  </p>
+            <h2>How to contact us</h2>
+            <p>If you have any questions about Anthology's privacy policy, the data we hold on you, or you would like to exercise one of your data protection rights, #
+                please do not hesitate to contact us. </p>
+            <p>Email us at: LXM153@student.bham.ac.uk </p>
+            <p>Call us: 07982 640245 </p>
+            <p>Or write to us at:</p>
+            <p>11 Rookery Road<br/>
+               Selly Oak <br/>
+               B29 7DG <br/>
+               United Kingdom
+            </p>
+        </div>
+    )
+}
+
+export default GDPR
