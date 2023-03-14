037c717bc316793c3c4a9346fe93fd9c40dc2ccc
Holly Packer <hjp183@student.bham.ac.uk>
2023-03-14 18:16:37 +0000

    fix: CSRF token not being included

--- poemfrontend/src/js/Api.js
+++ poemfrontend/src/js/Api.js
@@ -5,7 +5,11 @@ import Cookies from 'universal-cookie';
 const api = axios.create({
     baseURL: process.env.REACT_APP_API_ENDPOINT || 'http://localhost/api',
     transformRequest: [(data, headers) => {
-        headers['Authorization'] = `Token ${new Cookies().get("Token")}`
+
+        const cookies = new Cookies();
+
+        headers['Authorization'] = `Token ${cookies.get("Token")}`
+        headers[`X-CSRFToken`] = cookies.get("csrftoken");
 
         return data;
     }, ...axios.defaults.transformRequest]
