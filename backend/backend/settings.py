14751c3b3580d422e7cd6c49d9396b9153e16e9b
Holly Packer <hjp183@student.bham.ac.uk>
2023-03-24 20:19:01 +0000

    fix: five six seven eight oh how much i want to give cors hate

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,3 +139,8 @@ STATIC_URL = 'django-admin/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+   'https://team16-22.bham.team',
+   'https://anthology.bham.team'
+]
