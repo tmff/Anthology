b32363e5143aac234c5e2c6460ba63acdd4bcfc9
lilo16 <NAME>
2023-04-19 20:40:04 +0100

    undo react-speech-kit

--- poemfrontend/src/components/PoemViewer.jsx
+++ poemfrontend/src/components/PoemViewer.jsx
@@ -5,9 +5,9 @@ import '../css/Poem.css'
 import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCommentDots, faHeart as faSolidHeart, faPaperPlane, faUserCircle, faBookmark as faSolidBookmark, faVolumeHigh} from '@fortawesome/free-solid-svg-icons';
+import { faCommentDots, faHeart as faSolidHeart, faPaperPlane, faUserCircle, faBookmark as faSolidBookmark } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faRegularHeart, faBookmark as faRegularBookmark } from '@fortawesome/free-regular-svg-icons';
-import {useSpeechSynthesis} from 'react-speech-kit';
+// import {useSpeechSynthesis} from 'react-speech-kit';
 import axios from "axios";
 
 //Pass in id of poem that you want to get
@@ -19,7 +19,7 @@ export const PoemViewer = (props) => {
     const [bookmarked, setBookmarked] = useState(false);
     const [likes, setLikes] = useState(0)
     const [comments, setComments] = useState(0)
-    const {speak} = useSpeechSynthesis();
+    // const {speak} = useSpeechSynthesis();
 
     useEffect(() => {
         const cancelToken = axios.CancelToken.source();
@@ -82,12 +82,12 @@ export const PoemViewer = (props) => {
         setBookmarked(!bookmarked);
     }
 
-    function readPoem () {
-        speak({text: title, rate: 0.85})
-        speak({text: line1, rate: 0.85})
-        speak({text: line2, rate: 0.85})
-        speak({text: line3, rate: 0.85})
-    }
+    // function readPoem () {
+    //     speak({text: title, rate: 0.85})
+    //     speak({text: line1, rate: 0.85})
+    //     speak({text: line2, rate: 0.85})
+    //     speak({text: line3, rate: 0.85})
+    // }
     
     return(
         <div data-testid="poem-viewer">
@@ -108,7 +108,7 @@ export const PoemViewer = (props) => {
                 {likes}
                 <FontAwesomeIcon icon={faCommentDots} className="button-icon" data-tooltip-id="comment-tooltip" />
                 {comments}
-                <FontAwesomeIcon icon = {faVolumeHigh} className="button-icon" data-tooltip-id= "speech-tooltip" onClick= {readPoem} />
+                {/* <FontAwesomeIcon icon = {faVolumeHigh} className="button-icon" data-tooltip-id= "speech-tooltip" onClick= {readPoem} /> */}
 
                 <FontAwesomeIcon icon={faPaperPlane} className="button-icon" data-tooltip-id="share-tooltip" />
 
@@ -116,7 +116,7 @@ export const PoemViewer = (props) => {
                 <Tooltip id="comment-tooltip" content="Comments" />
                 <Tooltip id="share-tooltip" content="Share" />
                 <Tooltip id="bookmark-tooltip" content="Bookmark" />
-                <Tooltip id="speech-tooltip" content= "Read poem out loud" />
+                {/* <Tooltip id="speech-tooltip" content= "Read poem out loud" /> */}
                 </div>
             </div>
             </div>
