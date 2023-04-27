08a7f3624a0694eb61a08bcd6fa5e64223143fa0
Sam <scr145@student.bham.ac.uk>
2023-04-27 16:09:09 +0100

    feat:added logout finally

--- poemfrontend/src/components/Sidebar.js
+++ poemfrontend/src/components/Sidebar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import '../css/Sidebar.css';
+import Cookies from 'universal-cookie'
+
 
 export default props => {
     
+    const cookies = new Cookies();
+
+    function DeleteLoginToken(){
+        cookies.remove('Token')
+        useNavigate().navigate('/login')
+    }
+
     return (
         <Menu>
             <Link to={`/friends`} className='menu-item'>Friends</Link>
@@ -16,6 +26,7 @@ export default props => {
             <Link to={`/edit-profile`} className='menu-item'>Edit Profile</Link>
 	        <Link to={`/privacy`} className='menu-item'>Privacy Policy</Link>
             <Link to={`/bookmarks`} className='menu-item'>Bookmarks</Link>
+            <Link to={`/`} className='menu-item' onClick={DeleteLoginToken}>Logout</Link>
         </Menu>
     );
     
